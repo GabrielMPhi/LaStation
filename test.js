a67516cd72381dd29e0820691f1e1e4cbd9f94fd
Phieldar <gabriel.monet@gmail.com>
2020-06-26 21:02:17 -0400

    Corrections

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 (function (){
 
   var Cite = {
-      nom: "Montreal",
+      nom: "Monreal",
       dirigeant: "",
       richesse: 10,
+      moral:10, 
   }
 
   let tour = 1
@@ -13,41 +14,47 @@
   nouveauNom = nomsDeLaCite[Math.floor(Math.random() * nomsDeLaCite.length)];
   Cite.nom = nouveauNom
   console.log(Cite.nom)
-  nomsPersonnages = ["Catherine", "Bernard", "Rapunzel", "Sophia", "Martin", "Gabriel"]
+  nomsPersonnages = ["Catherine", "Bernard", "Rapunzel", "Sophia", "Martin", "Gabriel", "Mathieu", "Arthur", "Rieke", "Christina", "Christian"]
   let nomChoisit = nomsPersonnages[Math.floor(Math.random() * nomsPersonnages.length)];
   Cite.dirigeant = nomChoisit
 
   console.log(Cite)
 
-
-  document.getElementById('tourinfo').textContent = tour
-  document.getElementById('descriptionCitePage').textContent = Cite.nom
-  document.getElementById('consulCite').textContent = nomChoisit
-  partie()
-
-  function partie() {
-    document.querySelector('#formchoix').addEventListener('submit', function (e){
-      var choix = document.querySelector('#actionchoix').selectedOptions[0].value
-      console.log(choix)
-      switch(choix) {
-        case "fete":
-          alert("C'est la fête et la danse!");
-          tour++
-        break;
-      case "commerce":
-        tour++
-          alert("C'est jour de marché!");
-        break;
-      default:
-          alert("Ok!");
-          tour ++
-        } 
-      console.log(e)
-      console.log(tour)
-      document.getElementById('tourinfo').textContent = tour
-      partie()
-      })
+  //garde la description
+  const keepDescription = () => {
+    document.getElementById('descriptionCitePage').textContent = Cite.nom;
+    document.getElementById('consulCite').textContent = nomChoisit;
   }
+
+  document.getElementById('tourinfo').textContent = tour;
+
+  document.querySelector('#formchoix').addEventListener('submit', function (e){
+    var choix = document.querySelector('#actionchoix').selectedOptions[0].value
+    console.log(choix)
+    switch(choix) {
+      case "fete":
+        alert("C'est la fête et la danse!");
+        tour++
+        Cite.richesse--
+        Cite.moral++
+      break;
+    case "commerce":
+      tour++
+      Cite.richesse--
+      Cite.moral--
+        alert("C'est jour de marché!");
+      break;
+    default:
+        alert("Ok!");
+        tour ++
+      } 
+    console.log(e)
+    console.log(tour)
+    console.log(Cite.richesse)
+    console.log(Cite.moral)
+    document.getElementById('tourinfo').textContent = tour
+    keepDescription()
+    })
   /*
 class Partie {
 
