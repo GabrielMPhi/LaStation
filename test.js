7dbbb2c459a8bb37f20d91648c3d864b2fa9b132
BerDuc <bernard_ducharme@yahoo.ca>
2020-06-27 15:57:59 -0400

    modifications: ajout de css, ajout d'une classe cité, gestion des écrans, création de la cité, réglé le bogue de rechargement de la page

--- test.js
+++ test.js
@@ -1,60 +1,144 @@
-(function (){
+//VARIABLES GLOBALES
+var liste_type_gouv = ["monarchie", "republique"];
+let nomsDeLaCite = ["Montreal", "Laval", "Candiac"]
+nomsPersonnages = ["Catherine", "Bernard", "Rapunzel", "Sophia", "Martin", "Gabriel", "Mathieu", "Arthur", "Rieke", "Christina", "Christian"]
+let tour = 1
+var cite_joueur;
+var cite_ordi;
 
-  var Cite = {
-      nom: "Monreal",
-      dirigeant: "",
-      richesse: 10,
-      moral:10, 
+class Cite {
+
+  constructor(nom, regime){
+    this._nom = nom;
+    this._regime = regime;
+    this._dirigeant = choisir_nom_personnage();
+    this._richesse = 10;
+    this._moral = 10;
   }
 
-  let tour = 1
+  get nom() {
+    return this._nom
+  }
+  set nom(nom){
+    this._nom = nom
+  }
+  get regime(){
+    return this._regime;
+  }
+  set regime (regime){
+    this._regime = regime;
+  }
+  get dirigeant() {
+    return this._dirigeant
+  }
+  set dirigeant (dirigeant){
+    this._dirigeant= dirigeant;
+  }
+  get richesse(){
+    return this._richesse;
+  }
+  set richesse(richesse){
+    this._richesse = richesse;
+  }
+  get moral(){
+    return this._moral;
+  }
+  set moral(moral){
+    this._moral = moral;
+  }
 
-  console.log(Cite.nom)
-  let nomsDeLaCite = ["Montreal", "Laval", "Candiac"]
-  nouveauNom = nomsDeLaCite[Math.floor(Math.random() * nomsDeLaCite.length)];
-  Cite.nom = nouveauNom
-  console.log(Cite.nom)
-  nomsPersonnages = ["Catherine", "Bernard", "Rapunzel", "Sophia", "Martin", "Gabriel", "Mathieu", "Arthur", "Rieke", "Christina", "Christian"]
-  let nomChoisit = nomsPersonnages[Math.floor(Math.random() * nomsPersonnages.length)];
-  Cite.dirigeant = nomChoisit
+}
 
-  console.log(Cite)
+function choisir_nom_personnage(){
+  return nomsPersonnages[Math.floor(Math.random() * nomsPersonnages.length)]
+}
 
+/*CRÉATION DE LA PAGE*/
+window.onload = function(){
+  generer_listeHTML_gouvernements(liste_type_gouv);
+}
+
+function generer_listeHTML_gouvernements(liste){
+ 
+  var liste_html = document.getElementById("select_type_gouv");
+  liste.forEach( opt => {
+    var option_html = document.createElement("option");
+    option_html.innerHTML = opt;
+    option_html.value = opt;
+    option_html.id = "gouv"+opt;
+    liste_html.appendChild(option_html);
+  })
+}
+
+/* GESTION DES ÉCRANS */
+function changeScreen(ecranDepart, ecranArrivee){
+	document.getElementById(ecranDepart).setAttribute('hidden', 'hidden');
+	document.getElementById(ecranArrivee).removeAttribute('hidden');
+}
+
+function charger_description_cite(ecran_de_depart){
+  afficherDescription();
+  changeScreen(ecran_de_depart, "ecran_description_cite");
+}
+
+
+
+/*Écran de description de la cité*/
   //garde la description
-  const keepDescription = () => {
-    document.getElementById('descriptionCitePage').textContent = Cite.nom;
-    document.getElementById('consulCite').textContent = nomChoisit;
+function afficherDescription() {
+    document.getElementById('descriptionCitePage').textContent = cite_joueur.nom;
+    document.getElementById('consulCite').textContent = cite_joueur.dirigeant;
   }
 
-  document.getElementById('tourinfo').textContent = tour;
 
-  document.querySelector('#formchoix').addEventListener('submit', function (e){
-    var choix = document.querySelector('#actionchoix').selectedOptions[0].value
-    console.log(choix)
-    switch(choix) {
-      case "fete":
-        alert("C'est la fête et la danse!");
-        tour++
-        Cite.richesse--
-        Cite.moral++
-      break;
-    case "commerce":
+
+document.getElementById('tourinfo').textContent = tour;
+document.querySelector('#btnActionChoix').addEventListener('click', function (e){
+  var choix = document.querySelector('#actionchoix').selectedOptions[0].value
+  console.log(choix)
+  switch(choix) {
+    case "fete":
+      alert("C'est la fête et la danse!");
       tour++
       Cite.richesse--
-      Cite.moral--
-        alert("C'est jour de marché!");
-      break;
-    default:
-        alert("Ok!");
-        tour ++
-      } 
-    console.log(e)
-    console.log(tour)
-    console.log(Cite.richesse)
-    console.log(Cite.moral)
-    document.getElementById('tourinfo').textContent = tour
-    keepDescription()
-    })
+      Cite.moral++
+    break;
+  case "commerce":
+    tour++
+    Cite.richesse--
+    Cite.moral--
+      alert("C'est jour de marché!");
+    break;
+  default:
+      alert("Ok!");
+      tour ++
+    } 
+  console.log(e)
+  console.log(tour)
+  console.log(Cite.richesse)
+  console.log(Cite.moral)
+  document.getElementById('tourinfo').textContent = tour
+  keepDescription()
+  })
+
+
+/*Écran de création de la cité*/
+
+document.getElementById("btn_creation_cite").addEventListener('click', function (e){
+  var nom_cite = document.getElementById("txt_nom_cite").value
+  var regime_choisi = document.getElementById("select_type_gouv").value
+  cite_joueur = new Cite(nom_cite, regime_choisi);
+  console.log("on a créé une cité! = "+cite_joueur);
+  charger_description_cite("ecran_creation_cite");
+});
+
+
+
+
+
+
+	
+	
   /*
 class Partie {
 
@@ -77,4 +161,3 @@ class Partie {
 */
 
 
-})() // fin de la window... si je comprends bien.
