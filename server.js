8615f6630aad9d60845501755932441bf4648e6a
BerDuc <bernard_ducharme@yahoo.ca>
2020-07-13 23:53:31 -0400

    on avait besoin d'un serveur pour faire des imports-exports en JS. j'en ai donc fait un petit avec Node.JS.

---
+++ server.js
@@ -0,0 +1,30 @@
+let http = require('http');
+let url = require('url');
+var fs = require('fs'); 
+
+let petit_serveur = function (req, res){
+    let q = url.parse(req.url, true);
+    //console.log(q); 
+    let filename = "."+q.pathname;
+    //console.log(filename); 
+
+    fs.readFile(filename, 
+    function (err, data) {
+        if (err){ res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+        }
+
+        if(req.url.endsWith('.html')){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+        }
+        if(req.url.endsWith('.js')){
+            res.writeHead(200, { 'Content-Type': 'application/javascript'})
+        }
+        res.write(data);
+        return res.end();
+    });    
+}
+
+let server = http.createServer(petit_serveur);
+
+server.listen(8080);
